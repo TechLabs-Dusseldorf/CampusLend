e3e72e2fb460734fa12eef573a1f4446f84edcf8
Rumas97 <rutulmaskar23@gmail.com>
2026-07-21 17:06:22 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # CampusLend
+
+## MVP
+
+### Who is this for?​
+
+
+
+### What single problem are we solving?​
+
+
+
+### What is the ONE thing the user must be able to do?​
+
+
+
+
+## Must Haves MVP:
+
+
+## Nice-to-have Features 
---
+++ frontend/eslint.config.js
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import {defineConfig} from "eslint/config";
+
+export default defineConfig([
+    {
+        ignores: ["dist/**"]
+    },
+    {
+        files: ["**/*.{js,mjs,cjs,jsx}"],
+        plugins: {js},
+        extends: ["js/recommended"],
+        languageOptions: {globals: globals.browser}
+    },
+    pluginReact.configs.flat.recommended,
+
+    {
+        files: ["**/*.{js,mjs,cjs,jsx}"],
+        settings: { react: { version: "detect" } },
+        rules: {
+            "react/react-in-jsx-scope": "off",
+            "react/prop-types": "off"
+        }
+    }
+]);
